f39d40c93e91ca4e94b4d886b6e6959a5497af74
Williamcl9 <68758576+Williamcl9@users.noreply.github.com>
2020-12-01 22:31:21 +0000

    Add files via upload

--- Assignment 2/Coursework2.m
+++ Assignment 2/Coursework2.m
@@ -2,4 +2,4 @@ T = readtable('data.csv');
 T = removevars(T,{'Var1'});
 features=removevars(T,{'Var2'});
 labels=table2array(T(:,1));
-best_attribute(features,labels)
+[attribute,threshold]=best_attribute(features,labels)
--- Assignment 2/best_attribute.m
+++ Assignment 2/best_attribute.m
@@ -1,9 +1,9 @@
 function [attribute,threshold]= best_attribute(feature,labels)
 all_threshold=[];
-for i=1:width(feature)
+for i=1:size(feature, 2)
     all_threshold(i)=best_threshold(table2array(feature(:,i)),labels);
 end
 [B, I]=max(all_threshold);
 attribute=I;
-[info_gain,threshold]= best_threshold(table2array(feature(:,I)),labels)
+[info_gain,threshold]= best_threshold(table2array(feature(:,I)),labels);
 end
