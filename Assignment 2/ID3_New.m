841cdd0ceab8969979d6d36b0ebaa2a9f9409298
Jay Maindorge <44149803+JMaindorge@users.noreply.github.com>
2020-12-01 21:53:28 +0000

    new

--- Assignment 2/ID3_New.m
+++ Assignment 2/ID3_New.m
@@ -1,40 +1,41 @@
-function tree = ID3_New(Features, Labels)
+function tree = ID3_New(Features)
     %Examples(~ismember(Examples.('radius_mean'), value), :) = []
     
     %If all examples are the same, return the same label
-    tempLabels = Convert(table2array(Labels(:,2)));
+    
+    tempLabels = Features(:,end);
     if all(tempLabels(:) == tempLabels(1))
         tree.prediction = tempLabels(1);
         return
     end
     
-    %a = InfoGain(Features, 31, Convert(table2array(Labels(:, 2))))
+    for i = 1:length(Features(:,end-1))            
+        InfoGain(Features, i, Features(:,end))
+    end
 
     %[best_attribute, best_threshold] = InfoGain(Features, Labels);
     %tree.attribute = best_attribute;
     %tree.threshold = best_threshold;
     
     best_attribute = 'radius_mean';
-    
+    best_threshold = 17.0;
     tree.op = best_attribute;
     
-    LeftSubSet = Features.(best_attribute) < 17.0;
-    LeftSubSet = Features(LeftSubSet, :);
+    LeftSubSet = Features(Features(:,2) < best_threshold, :);
        
     if ~isempty(LeftSubSet)
-        LeftSubSet = removevars(LeftSubSet, best_attribute);
-        LeftLabelsVi = Labels(ismember(LeftSubSet.('id'), Features.('id')), :);
+        LeftSubSet = Features;
+        LeftSubSet(:, best_attribute) = [];
             
-        tree.kids(1) = ID3_New(LeftSubSet, LeftLabelsVi);
+        tree.kids(1) = ID3_New(LeftSubSet, LeftLabels);
     end
     
-    RightSubSet = Features.(best_attribute) >= 17.0;
-    RightSubSet = Features(RightSubSet, :);
+    RightSubSet = Features(Features(:,2) >= best_threshold, :);
         
     if ~isempty(RightSubSet)
-        RightSubSet = removevars(RightSubSet, best_attribute);
-        RightLabelsVi = Labels(ismember(RightSubSet.('id'), Features.('id')), :);
+        RightSubSet = Features;
+        RightSubSet(:, best_attribute) = [];
             
-        tree.kids(2) = ID3_New(RightSubSet, RightLabelsVi);
+        tree.kids(2) = ID3_New(RightSubSet, RightLabels);
     end
 end
