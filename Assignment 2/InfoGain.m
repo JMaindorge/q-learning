6134bda60265758f38b2b7073a77c14bfa942b36
Usama Zulfiqar <51174055+Gitter39@users.noreply.github.com>
2020-12-01 19:25:34 +0000

    Add files via upload

--- Assignment 2/InfoGain.m
+++ Assignment 2/InfoGain.m
@@ -2,12 +2,16 @@ function [information_gain] = InfoGain(data, split_feature_num, label)
 
 total_entropy = entropy(label);
 
-values = unique(data(:,split_feature_num));
+[values, index] = unique(data(:,split_feature_num));
 count = histcounts(data(:,split_feature_num), values);
 
 for i = 1:length(values)
     
-    weighted_entropy = sum((count(i)/sum(count)) * entropy(data(:,split_feature_num)))
-
+    weighted_entropy = sum((count(i)/sum(count)) * entropy(lable(index)));
+    
 end
+
+information_gain = total_entropy - weighted_entropy;
+
+
 
