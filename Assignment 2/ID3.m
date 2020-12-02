d197d008207616f190e43236806d1701969779c5
Usama Zulfiqar <51174055+Gitter39@users.noreply.github.com>
2020-12-02 00:47:32 +0000

    Add files via upload

--- Assignment 2/ID3.m
+++ Assignment 2/ID3.m
@@ -1,13 +1,11 @@
-function tree = ID3(data, originalData, feature_num, parent_node_mode)
+function tree = ID3(data, originalData, features, parent_node_mode)
     %Create a node
-    tree.op = Testing_Attribute;
     tree.kids = cell(1,2);
     tree.prediction = []; %Array holding 0 or 1 for leaf node. [] for internal.
     tree.attribute = []; %The value of the attribute being tested
     tree.threshold = []; %The threshold value determined to split.
     
     label = data(:,end); %label column from data
-%     features = data(:,end - 1); %features in our data
     
     %If all examples are the same, return the same label
     if length(unique(label)) == 1
@@ -26,6 +24,18 @@ function tree = ID3(data, originalData, feature_num, parent_node_mode)
     end
         
     parent_node_mode = mode(data(:,end));
+    
+    feature_ents = zeros(1,width(features));
+    
+    for feature = 1:width(features)
+        feature_ents(feature) = InfoGain(data, feature, label);
+    end
+    
+    best_attribute_col = (feature_ents == max(feature_ents));
+    tree.attribute = features(:,best_attribute_col);
+    
+    tree.threshold = best_threshold(features(:,best_attribute_col), label);
+    features(:,best_attribute_col) = []; %drop best column.
     
     
     
