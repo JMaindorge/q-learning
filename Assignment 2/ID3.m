a8b18399290c58e604820be3bca2140bf8d382df
Usama Zulfiqar <51174055+Gitter39@users.noreply.github.com>
2020-12-02 18:01:36 +0000

    Add files via upload

--- Assignment 2/ID3.m
+++ Assignment 2/ID3.m
@@ -1,42 +1,43 @@
-function tree = ID3(data, originalData, features, parent_node_mode)
-    %Create a node
+function tree = ID3(features, labels, parent_mode, feature_names)
+    parent_mode  = mode(labels);
+    %cell to hold subtrees
     tree.kids = cell(1,2);
-    tree.prediction = []; %Array holding 0 or 1 for leaf node. [] for internal.
-    tree.attribute = []; %The value of the attribute being tested
-    tree.threshold = []; %The threshold value determined to split.
-    
-    label = data(:,end); %label column from data
     
     %If all examples are the same, return the same label
-    if length(unique(label)) == 1
-        tree.prediction = unique(label);
+    if length(unique(labels)) == 1
+        tree.kids = [];
+        tree.prediction = unique(labels);
     end
-    
-    %If predicting examples are empty, return the most common value from
-    %the attribute
-    if isempty(data)
-        tree.prediction = mode(originalData(:,end));
-        return
-    end
-    
-    if isempty(features)
-        tree.prediction = parent_node_mode;
-    end
-        
-    parent_node_mode = mode(data(:,end));
-    
+         
     feature_ents = zeros(1,width(features));
     
     for feature = 1:width(features)
-        feature_ents(feature) = InfoGain(data, feature, label);
+        feature_ents(feature) = InfoGain(features, feature, labels);
     end
     
-    best_attribute_col = (feature_ents == max(feature_ents));
-    tree.attribute = features(:,best_attribute_col);
+    [col, best_att_num] = max(feature_ents);
+    tree.attribute = best_att_num;
+    tree.op = feature_names{best_att_num};
+    tree.threshold = best_threshold(features(:,best_att_num), labels);
     
-    tree.threshold = best_threshold(features(:,best_attribute_col), label);
-    features(:,best_attribute_col) = []; %drop best column.
+    left_tree_features = features(features(:,best_att_num) < tree.threshold, :);
+    left_tree_labels = labels(features(:,best_att_num) < tree.threshold);
+    left_tree_features(:,best_att_num) = [];
     
+    if ~isempty(left_tree_features)
+        tree.kids{1} = ID3(left_tree_features, left_tree_labels, parent_mode, feature_names);
+    else
+        tree.prediction = parent_mode;
+    end
     
+    right_tree_features = features(features(:,best_att_num) >= tree.threshold, :);
+    right_tree_labels = labels(features(:,best_att_num) >= tree.threshold);
+    right_tree_features(:,best_att_num) = [];
+    
+    if ~isempty(right_tree_features)
+        tree.kids{2} = ID3(right_tree_features, right_tree_labels, parent_mode, feature_names);
+    else
+        tree.prediction = parent_mode;
+    end 
     
 end
