edefd5d78331065325746aaee6cf49a1547d47fb
Jay Maindorge <44149803+JMaindorge@users.noreply.github.com>
2020-12-02 20:48:13 +0000

    Add files via upload

--- Assignment 2/CrossValidation.m
+++ Assignment 2/CrossValidation.m
@@ -1,9 +1,10 @@
 function result = CrossValidation(chunks, size)
     sampleSize = floor(size / chunks);
+    result = zeros(1,chunks);
     for i=1:chunks
         %val = randi([1 513]);
         %result(i) = DecisionTree(val, val+56);
-        result = zeros(1,chunks);
+        %
         if i==1
             result(i) = DecisionTree(1,sampleSize);
         else
